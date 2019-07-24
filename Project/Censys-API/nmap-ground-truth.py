cd5634754c4ad80925df2646a4613ddb2a85dbbc
HaseebLUMS <haseeb.lums@gmail.com>
2019-07-25 01:15:00 +0500

    ip with banners

---
+++ Project/Censys-API/nmap-ground-truth.py
@@ -0,0 +1,39 @@
+import json
+import sys
+import os
+
+with open('banners.json') as f: data = f.read()
+
+mapping = {}
+
+
+data = json.loads(data)
+
+ips = ['197.253.22.108']
+
+for ele in data:
+	ips.append(data[ele]["ip"])
+
+print(ips)
+
+myCmd = 'sudo nmap -sV -T4 '
+
+for ip in ips:
+	try:
+		cmd = myCmd + str(ip)
+		res = os.popen(cmd).read()
+		print(res)
+		lines = res.split('\n')
+		ans = ''
+		for l in lines:
+			if 'Service Info' in l:
+				ans = l
+				break
+		mapping[ip] = ans
+	except:
+		pass
+
+with open("mapping.json", 'w') as f:
+	mapping = json.dumps(mapping, indent=4)
+	f.write(mapping) 
+
