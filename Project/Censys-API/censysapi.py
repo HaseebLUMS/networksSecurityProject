3721d604d82a16498ee7371768629bed8fbe8e13
HaseebLUMS <haseeb.lums@gmail.com>
2019-07-25 00:49:49 +0500

    ip with banners

--- Project/Censys-API/censysapi.py
+++ Project/Censys-API/censysapi.py
@@ -2,7 +2,7 @@ import censys.ipv4
 import json
 c = censys.ipv4.CensysIPv4(api_id="425bdc11-6c8d-4f8a-a3ee-e9b1201dd288", api_secret="LavM4jWU3aeh2Tt7OhchmYAtLnWDqA3U")
 
-IPV4_FIELDS = ['21.ftp.banner.banner', '23.telnet.banner.banner', '80.http.get.title']
+IPV4_FIELDS = ['ip', '21.ftp.banner.banner', '23.telnet.banner.banner', '80.http.get.title']
 
 data = (c.search('((protocols: "80/http") AND protocols: "21/ftp") AND protocols: "23/telnet"', 
                              IPV4_FIELDS, max_records=1000))	
@@ -11,19 +11,19 @@ banners = {}
 i = 0
 for d in data:
     try:
-        banners[i] = {'banner': d['21.ftp.banner.banner'], 'protocol': 'FTP'}
+        banners[i] = {'banner': d['21.ftp.banner.banner'], 'protocol': 'FTP', 'ip': d['ip']}
         i += 1
     except:
         print('no ftp')
         pass
     try:
-        banners[i] = {'banner': d['23.telnet.banner.banner'], 'protocol': 'TELNET'}
+        banners[i] = {'banner': d['23.telnet.banner.banner'], 'protocol': 'TELNET', 'ip': d['ip']}
         i += 1
     except:
         print('no telnet')
         pass
     try:
-        banners[i] = {'banner': d['80.http.get.title'], 'protocol': 'HTTP'}
+        banners[i] = {'banner': d['80.http.get.title'], 'protocol': 'HTTP', 'ip': d['ip']}
         i += 1
     except:
         print('no http')
