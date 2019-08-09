37358c8fee1f7774e43db4dd157793b9c7d26792
HaseebLUMS <haseeb.lums@gmail.com>
2019-08-09 20:07:59 +0500

    new rules

--- Project/apriori_algorithm/testApriori.py
+++ Project/apriori_algorithm/testApriori.py
@@ -49,7 +49,7 @@ def contains_prediction(a):
 --writes the inferred rule in file "RULES"
 '''
 def run_apriori(trans):
-	results = list(apriori(trans, min_support=0.0005, min_confidence=0.01) ) 
+	results = list(apriori(trans, min_support=0.005, min_confidence=0.01) ) 
 
 	print(len(results))
 	df = pd.DataFrame(columns=('Items','Support','Confidence'))
